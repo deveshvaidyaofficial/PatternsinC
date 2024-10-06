6b0a47d5f83c9bc4b764654e99d144d5304ed8c8
Asmi1108 <asmisaner08@gmail.com>
2024-10-06 19:13:06 +0530

    Create HollowTriangle_Pattern.c

---
+++ HollowTriangle_Pattern.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+void main()
+{
+    for (int i = 0; i <5; i++) 
+     {
+        for (int j = 0; j < 2 * (5 - i) - 1; j++) 
+        {
+            printf(" ");
+        }
+        for (int k = 0; k < 2 * i + 1; k++)
+       {
+            if (k==0 || k==2 * i || i==5- 1) 
+            {
+                printf("* ");
+            }
+            else 
+            {
+                printf("  ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
